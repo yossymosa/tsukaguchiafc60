20fe88e7f7405bf6659705439d02170f7519cff1
yossymosa <yossymosa@gmail.com>
2026-03-22 01:34:25 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tsukaguchi-afc-v1";
+const CACHE_NAME = "tsukaguchi-afc-v2";
 const APP_SHELL = ["/", "/index.html", "/manifest.json", "/icon-512.png"];
 
 self.addEventListener("install", event => {
